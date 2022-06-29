b78e2a5f59331e6596a9c00fda5bd15d7ee2a603
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-28 20:11:06 -0700

    method to find the geratest subarray of sum zero

--- src/LargestSubarrayWith0Sum.java
+++ src/LargestSubarrayWith0Sum.java
@@ -1,6 +1,24 @@
+import java.util.HashMap;
+
 public class LargestSubarrayWith0Sum {
 
-    public static int largest
+    public static int largestSubArrayWith0Sum(int arr[], int n){
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0, max = 0;
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+            if(sum ==0){
+                max = Math.max(max, i+1);
+            }
+            if(map.containsKey(sum)){
+                max = Math.max(max, i-map.get(sum));
+            }
+            else {
+                map.put(sum, i);
+            }
+        }return max;
+    }
 
     public static void main(String[] args) {
 
